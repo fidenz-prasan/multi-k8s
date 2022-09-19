f64e6495f34c94b997bacfe644e89d84745c70f2
fidenz-prasan <prasan.n@sms.fidenz.com>
2022-09-19 10:33:03 +0530

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t nimanthaprasan/multi-client:latest -t nimanthaprasan/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nimanthaprasan/multi-server:latest -t nimanthaprasan/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nimanthaprasan/multi-worker:latest -t nimanthaprasan/multi-server:$SHA -f ./worker/Dockerfile ./worker
+
+docker push nimanthaprasan/multi-client:latest
+docker push nimanthaprasan/multi-server:latest
+docker push nimanthaprasan/multi-worker:latest
+
+docker push nimanthaprasan/multi-client:$SHA
+docker push nimanthaprasan/multi-server:$SHA
+docker push nimanthaprasan/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=nimanthaprasan/multi-server:$SHA
+kubectl set image deployment/client-deployment client=nimanthaprasan/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=nimanthaprasan/multi-worker:$SHA
